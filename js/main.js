d52ed074c6b9de01d5c91ef5d6fb924ff8f6ddca
Бугров Александр Алексеевич <gr433_buaal@ttit.local>
2025-02-12 11:32:45 +0700

    step 3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
-let app = new Vue({
-    el: '#app',
+Vue.component('Card', {
+    template: `
+    <div class="card">
+        <textarea v-model="card.content" placeholder="Введите заметку"></textarea>
+        <button @click="move">Переместить</button>
+    </div>
+    `,
+    props: {
+        card: Object,
+    },
+    methods: {
+        move() {
+            this.$emit('move', this.card.id);
+        },
+    },
+})
 
+Vue.component('column', {
+    template: `
+    <div class="column">
+        <div v-for="card in cards" :key="card.id">
+          <Card :card="card" @move="handleMove(card.id)" />
+        </div>
+        <button v-if="cards.length < maxCards" @click="addCard">Добавить карточку</button>
+    </div>
+    `,
+
+    props: {
+        cards: Array,
+        maxCards: Number,
+    },
+    methods: {
+        addCard() {
+            this.$emit('add-card');
+        },
+        handleMove(cardId) {
+            this.$emit('move-card', { cardId, fromColumnIndex: this.$vnode.key, toColumnIndex: null });
+        },
+    },
 })
 
 Vue.component('notepad', {
     template: `
-        <div class="app">
+        <div class="notepad">
             <column
                 v-for="(column, index) in columns"
                 :key="index"
@@ -40,45 +76,11 @@ Vue.component('notepad', {
     },
 })
 
-Vue.component('column', {
-    template: `
-    <div class="column">
-        <div v-for="card in cards" :key="card.id">
-          <Card :card="card" @move="handleMove(card.id)" />
-        </div>
-        <button v-if="cards.length < maxCards" @click="addCard">Добавить карточку</button>
-    </div>
-    `,
-    components: {
-        Card,
-    },
-    props: {
-        cards: Array,
-        maxCards: Number,
-    },
-    methods: {
-        addCard() {
-            this.$emit('add-card');
-        },
-        handleMove(cardId) {
-            this.$emit('move-card', { cardId, fromColumnIndex: this.$vnode.key, toColumnIndex: null });
-        },
-    },
-})
 
-Vue.component('Card', {
-    template: `
-    <div class="card">
-        <textarea v-model="card.content" placeholder="Введите заметку"></textarea>
-        <button @click="move">Переместить</button>
-    </div>
-    `,
-    props: {
-        card: Object,
-    },
-    methods: {
-        move() {
-            this.$emit('move', this.card.id);
-        },
-    },
+
+
+
+let app = new Vue({
+    el: '#app',
+
 })
